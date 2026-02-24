1fa3a858352554c593914ef5eba867e30a4a12b4
badideainc <untitledprogrammer@gmail.com>
2026-02-24 19:35:13 +0000

    Refactor gravity function to use dynamic window position and improve acceleration calculation

--- gravity_window.py
+++ gravity_window.py
@@ -1,11 +1,17 @@
 import pygetwindow as gw
+import pyautogui as pag
+import time
 
 def gravity(window):
 
+    start_time = time.perf_counter()
+
     while window:
-        if (window.bottomright[1] < 2000):
-            window.move(0, 1)
+        if (window.bottomright[1] < pag.size()[1]):
+            window.moveRel(0, int(acceleration(time.perf_counter() - start_time)))
+
+def acceleration(time):
+    return 0.5 * 9.81 * time**2
 
 win = gw.getWindowsWithTitle('Untitled')[0]
-
 gravity(win)
