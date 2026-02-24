6da1096b39032ce0d5e2f5f68d493475fa58fdc6
badideainc <untitledprogrammer@gmail.com>
2026-02-24 20:01:25 +0000

    Add Component class with initialization and update method

---
+++ component.py
@@ -0,0 +1,9 @@
+
+class Component:
+    def __init__(self, id, window, startTime):
+        self.id = id
+        self.window = window
+        self.start_time = startTime
+    
+    def update(self):
+        pass
