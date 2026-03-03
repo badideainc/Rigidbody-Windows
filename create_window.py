793a03a0e575fde75d9df037b9c1400070a91f4d
Untitled Programmer <blairlucas448@gmail.com>
2026-03-03 12:07:47 +0000

    Update RigidbodyWindow initialization to use test.html and Path for HTML path

--- create_window.py
+++ create_window.py
@@ -1,9 +1,11 @@
 from rigidbody_window import RigidbodyWindow
 from screen_listener import ScreenListener
+from pathlib import Path
 
 listener = ScreenListener()
+html_path = Path(__file__).parent / "test.html"
 
-rb = RigidbodyWindow("notepad.exe", "Untitled - Notepad", ["GravityWindowComponent"])
+rb = RigidbodyWindow("test.html", "Test", html_path, ["GravityWindowComponent"])
 print(rb)
 
 listener.add_window(rb)
