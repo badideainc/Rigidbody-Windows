1bd0bf87887198d7789903029adfbaef2e11eff8
Untitled Programmer <blairlucas448@gmail.com>
2026-03-03 11:48:38 +0000

    Reformat Distance method in Vector2 class to correct indentation

--- window_math.py
+++ window_math.py
@@ -3,6 +3,6 @@ class Vector2:
         self.x = x
         self.y = y
 
-@staticmethod
-def Distance(v1, v2):
-    return ((v1.x - v2.x)**2 + (v1.y - v2.y)**2)**0.5
+    @staticmethod
+    def Distance(v1, v2):
+        return ((v1.x - v2.x)**2 + (v1.y - v2.y)**2)**0.5
