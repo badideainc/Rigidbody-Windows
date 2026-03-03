1cddb6d4b3aea5d5d6632f05226b25640b607e24
Untitled Programmer <blairlucas448@gmail.com>
2026-03-03 09:16:50 +0000

    Initialize ScreenListener and integrate it with RigidbodyWindow

--- create_window.py
+++ create_window.py
@@ -1,4 +1,10 @@
 from rigidbody_window import RigidbodyWindow
+from screen_listener import ScreenListener
+
+listener = ScreenListener()
 
 rb = RigidbodyWindow("notepad.exe", "Untitled - Notepad", ["GravityWindowComponent"])
 print(rb)
+
+listener.add_window(rb)
+listener.listen()
