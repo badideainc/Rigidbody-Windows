4f151a9088e8c310d708c274dab8d663a9b33017
Untitled Programmer <blairlucas448@gmail.com>
2026-03-03 11:17:32 +0000

    Add WindowClickEvent and Vector2 classes to handle click events and positions

--- window_event.py
+++ window_event.py
@@ -6,3 +6,13 @@ class WindowCollisionEvent(WindowEvent):
     def __init__(self, window, other_window):
         super().__init__(window)
         self.other_window = other_window
+
+class WindowClickEvent(WindowEvent):
+    def __init__(self, window, x, y):
+        super().__init__(window)
+        self.position = Vector2(x, y)
+
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
