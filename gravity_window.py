580f0752214b9adfcf5bb39c595474e155f7dbca
Untitled Programmer <blairlucas448@gmail.com>
2026-03-03 12:54:19 +0000

    Refactor GravityWindowComponent to improve position and size handling

--- gravity_window.py
+++ gravity_window.py
@@ -1,19 +1,18 @@
-import pygetwindow as gw
-import pyautogui as pag
 import time
 from component import Component
 
 class GravityWindowComponent(Component):
-    def __init__(self, window, screen, startTime):
-        super().__init__("Gravity", window, screen, startTime)
+    def __init__(self, id, window, screen, startTime):
+        super().__init__(id, window, screen, startTime)
 
         self._gravity_constant = 9.81
 
         self._start_time = time.perf_counter()
 
-    def _gravity(self, window):
-        if (window.y + window.height < self.screen.height):
-                window.moveRel(0, int(self._acceleration(time.perf_counter() - self._start_time)))
+    def _gravity(self):
+        if (self.window.y + self.window.height < self.screen.height):
+                dy = int(self._acceleration(time.perf_counter() - self._start_time))
+                self.window.move(self.window.x, self.window.y + dy)
             
     def onClick(self, event):
         self._start_time = time.perf_counter()
@@ -22,4 +21,4 @@ class GravityWindowComponent(Component):
         return 0.5 * self._gravity_constant * time**2
     
     def update(self):
-        self._gravity(self.window)
+        self._gravity()
